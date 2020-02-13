8e51a14194fe080665675a65bb0b5962c6ec53ee
Rafał Meisel <rafal.n.meisel@gmail.com>
2020-02-13 01:57:46 +0100

    Change motive from gallery to library

---
+++ library/api/serializers.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers
+
+from library.api.models import Book
+
+
+class BookSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Book
+        fields = ['id', 'title', 'description', 'author', 'year']
