7792820ae42fb23085752aa94c8a6af58b715ede
Rafał Meisel <rafal.n.meisel@gmail.com>
2020-02-13 19:32:10 +0100

    Change gallery to library

---
+++ library/api/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+    author = models.CharField(max_length=64)
+    year = models.IntegerField()
---
+++ library/api/views.py
@@ -0,0 +1,12 @@
+from rest_framework import viewsets
+
+from .models import Book
+from .serializers import BookSerializer
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
